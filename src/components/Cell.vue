9a656c87f709b4bda8c2efab038b284b7625ad92
Franc Auxach Cortés <franc@analyticalls.com>
2020-11-30 17:33:46 +0100

    Added initial cell functionallity on click

--- src/components/Cell.vue
+++ src/components/Cell.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="bg-gray-800 h-full text-white flex justify-center items-center">{{ cell }}</div>
+  <div @click="toggleMark" class="cell select-none text-9xl bg-gray-800 h-full text-white flex justify-center items-center cursor-pointer">{{ mark }}</div>
 </template>
 
 <script lang="ts">
@@ -9,9 +9,20 @@ import { Options, Vue } from 'vue-class-component'
   name: 'Cell',
   props: ['cell']
 })
-export default class Cell extends Vue {}
+export default class Cell extends Vue {
+  mark = ''
+  options = ['x', '0']
+
+  private toggleMark () {
+    this.mark = this.options[Math.floor(Math.random() * this.options.length)]
+  }
+}
 </script>
 
-<style>
+<style lang="sass">
+@import url('https://fonts.googleapis.com/css2?family=Gochi+Hand&display=swap')
+
+.cell
+  font-family: 'Gochi Hand', cursive
 
 </style>
